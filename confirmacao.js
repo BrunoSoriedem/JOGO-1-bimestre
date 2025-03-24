c315f1835991feb642f942dd4ff7c317aa9f5528
Bruno Medeiros <brunorafamed.com@gmail.com>
2025-03-24 19:27:19 -0300

    amemmmm deu certo

--- confirmacao.js
+++ confirmacao.js
@@ -1,15 +1,7 @@
-// Seleciona o botão pelo id
 const enviarButton = document.getElementById('enviar-btn');
 
-// Adiciona um evento de clique ao botão
 enviarButton.addEventListener('click', function (event) {
     event.preventDefault();
-
-    enviarEmail();
-});
-
-
-function enviarEmail() {
 
     Swal.fire({
         position: "center",
@@ -17,6 +9,8 @@ function enviarEmail() {
         title: "E-mail enviado com sucesso!",
         showConfirmButton: false,
         timer: 1500
-    });
+    }).then(() => {
 
-}
+        document.querySelector('form').submit();
+    });
+});
