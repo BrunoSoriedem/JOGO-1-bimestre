ef1adf206c1eb4168ed398ad5102338602476a72
Bruno Medeiros <brunorafamed.com@gmail.com>
2025-03-24 19:35:24 -0300

    ta indoo

--- confirmacao.js
+++ confirmacao.js
@@ -1,16 +1,23 @@
-const enviarButton = document.getElementById('enviar-btn');
+const form = document.querySelector('form');
 
-enviarButton.addEventListener('click', function (event) {
+form.addEventListener('submit', function (event) {
+
     event.preventDefault();
 
-    Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "E-mail enviado com sucesso!",
-        showConfirmButton: false,
-        timer: 1500
-    }).then(() => {
+    if (form.checkValidity()) {
 
-        document.querySelector('form').submit();
-    });
+        Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "E-mail enviado com sucesso!",
+            showConfirmButton: false,
+            timer: 1500
+        }).then(() => {
+
+            form.submit();
+        });
+    } else {
+
+        form.reportValidity();
+    }
 });
