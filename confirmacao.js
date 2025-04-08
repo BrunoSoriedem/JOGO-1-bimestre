d91e1d937ad73bca2c583357e62318cc4c0dfa7f
Bruno Medeiros <brunorafamed.com@gmail.com>
2025-04-07 22:44:26 -0300

    alteracoes menu

--- confirmacao.js
+++ confirmacao.js
@@ -21,3 +21,26 @@ form.addEventListener('submit', function (event) {
         form.reportValidity();
     }
 });
+
+function formatarTelefone(input) {
+    const telefone = input.value.replace(/\D/g, '');
+
+    if (telefone.length < 10 || telefone.length > 12) {
+        input.setCustomValidity("Informe um número de telefone válido.");
+    } else {
+        input.setCustomValidity("");
+    }
+
+    if (telefone.length === 12) {
+        input.value = telefone.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    } else if (telefone.length === 10) {
+        input.value = telefone.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const telefoneInput = document.getElementById('telefone');
+    telefoneInput.addEventListener('input', function () {
+        formatarTelefone(this);
+    });
+});
