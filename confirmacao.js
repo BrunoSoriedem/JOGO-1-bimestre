43bdf557036d5516f0415630cda6ae153635cb50
Bruno Medeiros <brunorafamed.com@gmail.com>
2025-04-16 12:18:29 -0300

    teste

--- confirmacao.js
+++ confirmacao.js
@@ -1,46 +1,63 @@
-const form = document.querySelector('form');
+// INICIO NAVBAR
+const headerMenu = document.getElementById("header-menu");
+const navbarMobile = document.querySelector(".teste");
 
-form.addEventListener('submit', function (event) {
+headerMenu.addEventListener("click", function () {
+    setTimeout(function () {
+        navbarMobile.classList.toggle("active");
+    }, 500)
+})
 
-    event.preventDefault();
-
-    if (form.checkValidity()) {
-
-        Swal.fire({
-            position: "center",
-            icon: "success",
-            title: "E-mail enviado com sucesso!",
-            showConfirmButton: false,
-            timer: 1500
-        }).then(() => {
-
-            form.submit();
-        });
-    } else {
-
-        form.reportValidity();
+window.addEventListener("resize", function () {
+    if (window.innerWidth >= 870) {
+        navbarMobile.classList.remove("active");
     }
 });
+// FIM NAVBAR
 
-function formatarTelefone(input) {
-    const telefone = input.value.replace(/\D/g, '');
+// const form = document.querySelector('form');
 
-    if (telefone.length < 10 || telefone.length > 12) {
-        input.setCustomValidity("Informe um número de telefone válido.");
-    } else {
-        input.setCustomValidity("");
-    }
+// form.addEventListener('submit', function (event) {
 
-    if (telefone.length === 12) {
-        input.value = telefone.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
-    } else if (telefone.length === 10) {
-        input.value = telefone.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
-    }
-}
+//     event.preventDefault();
 
-document.addEventListener('DOMContentLoaded', () => {
-    const telefoneInput = document.getElementById('telefone');
-    telefoneInput.addEventListener('input', function () {
-        formatarTelefone(this);
-    });
-});
+//     if (form.checkValidity()) {
+
+//         Swal.fire({
+//             position: "center",
+//             icon: "success",
+//             title: "E-mail enviado com sucesso!",
+//             showConfirmButton: false,
+//             timer: 1500
+//         }).then(() => {
+
+//             form.submit();
+//         });
+//     } else {
+
+//         form.reportValidity();
+//     }
+// });
+
+// function formatarTelefone(input) {
+//     const telefone = input.value.replace(/\D/g, '');
+
+//     if (telefone.length < 10 || telefone.length > 12) {
+//         input.setCustomValidity("Informe um número de telefone válido.");
+//     } else {
+//         input.setCustomValidity("");
+//     }
+
+//     if (telefone.length === 12) {
+//         input.value = telefone.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+//     } else if (telefone.length === 10) {
+//         input.value = telefone.replace(/(\d{2})(\d{4})(\d{4})/, '($1) $2-$3');
+//     }
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const telefoneInput = document.getElementById('telefone');
+//     telefoneInput.addEventListener('input', function () {
+//         formatarTelefone(this);
+//     });
+// });
